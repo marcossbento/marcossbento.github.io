a901cb29772da24d7daa44be79c658b57d666783
marcossbento <marcossbento14@gmail.com>
2024-01-19 12:32:32 -0300

    minimal fixes

--- menu.js
+++ menu.js
@@ -15,9 +15,21 @@ overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
 })
 
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    var button = document.querySelector('.backtotop');
+
+    if (window.pageYOffset > header.offsetHeight) {
+        button.classList.add('show');
+    } else {
+        button.classList.remove('show');
+    }
+});
+
 const downloadButton = document.getElementById('downloadButton');
 downloadButton.addEventListener('click', downloadFile);
 
 function downloadFile() {
     window.open('images/CurriculumMarcosBento.pdf');
 }
+
