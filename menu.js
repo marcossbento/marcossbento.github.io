372e6a8847dd80e9f15277c3d27d54802aeb6ba9
marcossbento <marcossbento14@gmail.com>
2024-01-23 21:44:58 -0300

    Skills and texts changes

--- menu.js
+++ menu.js
@@ -33,3 +33,28 @@ function downloadFile() {
     window.open('images/CurriculumMarcosBento.pdf');
 }
 
+const scrollers = document.querySelectorAll(".logos");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+      // add data-animated="true" to every `.scroller` on the page
+      scroller.setAttribute("data-animated", true);
+  
+      // Make an array from the elements within `.scroller-inner`
+      const scrollerInner = scroller.querySelector(".logos-slide");
+      const scrollerContent = Array.from(scrollerInner.children);
+  
+      // For each item in the array, clone it
+      // add aria-hidden to it
+      // add it into the `.scroller-inner`
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    });
+  }
